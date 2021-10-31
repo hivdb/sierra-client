663152a3be0ead4cc95ec9d0c1139df437d2c829
Philip Tzou <philip.npc@gmail.com>
2021-10-31 14:34:47 -0700

    Update seqreads command line interface

--- python/sierrapy/commands/seqreads.py
+++ python/sierrapy/commands/seqreads.py
@@ -39,7 +39,7 @@ def normalize_gene(gene):
         return 'POL'
 
 
-def parse_seqreads(fp, min_prevalence=-1, min_read_depth=-1):
+def parse_seqreads(fp, min_prevalence=0, min_codon_count=0, min_read_depth=-1):
     gpmap = {}
     firstrow = fp.readline()
     delimiter = ','
@@ -85,6 +85,7 @@ def parse_seqreads(fp, min_prevalence=-1, min_read_depth=-1):
             gpmap.values(),
             key=lambda pcr: (VALID_GENES.index(pcr['gene']), pcr['position'])),
         'minPrevalence': min_prevalence,
+        'minCodonCount': min_codon_count,
         'minReadDepth': min_read_depth
     }
 
@@ -92,13 +93,15 @@ def parse_seqreads(fp, min_prevalence=-1, min_read_depth=-1):
 @cli.command()
 @click.argument('seqreads', nargs=-1, type=click.File('r'), required=True)
 @url_option('--url')
-@click.option('-p', '--cutoff', type=float, default=-1, show_default=True,
+@click.option('-p', '--pcnt-cutoff', type=float, default=0, show_default=True,
               help=('Minimal prevalence cutoff applied on the sequence reads '
                     '(range: 0-1.0)'))
+@click.option('-c', '--num-cutoff', type=int, default=0, show_default=True,
+              help='Minimal read count cutoff applied on the sequence reads.')
 @click.option('-d', '--min-read-depth', type=int, default=-1,
               show_default=True,
               help=('Minimal read depth applied to '
-                    'each codon of this sequence'))
+                    'each position of this sequence'))
 @click.option('-q', '--query', type=click.File('r'), show_default=True,
               help=('A file contains GraphQL fragment definition '
                     'on `SequenceAnalysis`.'))
@@ -106,7 +109,8 @@ def parse_seqreads(fp, min_prevalence=-1, min_read_depth=-1):
               show_default=True, help='File path to store the JSON result.')
 @click.option('--ugly', is_flag=True, help='Output compressed JSON result.')
 @click.pass_context
-def seqreads(ctx, url, seqreads, cutoff, min_read_depth, query, output, ugly):
+def seqreads(ctx, url, seqreads, pcnt_cutoff, num_cutoff,
+             min_read_depth, query, output, ugly):
     """
     Run alignment, drug resistance and other analysis for one or more
     tab-delimited text files contained codon reads of HIV-1 pol DNA sequences.
@@ -114,7 +118,9 @@ def seqreads(ctx, url, seqreads, cutoff, min_read_depth, query, output, ugly):
     client = SierraClient(url)
     client.toggle_progress(True)
 
-    seqreads = [parse_seqreads(fp, cutoff, min_read_depth) for fp in seqreads]
+    seqreads = [parse_seqreads(
+        fp, pcnt_cutoff, num_cutoff, min_read_depth
+    ) for fp in seqreads]
     if query:
         query = query.read()
     else:
